c50469276f25c8419a41c2bd6ec48d610904f263
VickThorInOz <vtm_32@hotmail.com>
2022-07-25 13:08:19 -0500

    end of section 11

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,12 +6,13 @@ import { IProductType } from '../shared/Models/ProductType';
 import {map} from 'rxjs/operators';
 import { ShopParams } from '../shared/Models/shopParams';
 import { IProduct } from '../shared/Models/Product';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
-  baseUrl = 'https://localhost:5001/api/'
+  baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
